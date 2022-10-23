95cf65d48a3ffab82036503bcb9a85f54dd10ce6
Arina Kohanova <arinakokhanova@gmail.com>
2022-10-23 13:30:49 +0300

    1. IB 2.  Part of the code

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var scrollingButton: UIButton!
+    
+    let currentAlpha: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = currentAlpha
+        yellowLightView.alpha = currentAlpha
+        greenLightView.alpha = currentAlpha
+        
+        scrollingButton.configuration = setUpButton(with: "Start")
+        
     }
 
+    
+    @IBAction func buttonTapped(by user: UIButton) {
+        
+        
+    }
+    
+    
+    private func setUpButton(with title : String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.title = title
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.910154283, green: 0.4194336236, blue: 0.6157357693, alpha: 1)
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        
+        return buttonConfiguration
+    }
 
 }
 
