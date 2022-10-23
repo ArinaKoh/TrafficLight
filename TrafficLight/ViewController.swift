7aeb0d5ce37066abdf48265e9e6448a606315aea
Arina Kohanova <arinakokhanova@gmail.com>
2022-10-23 14:51:40 +0300

    1. Set up button 2. Work with logic of the code

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -22,25 +22,42 @@ class ViewController: UIViewController {
         redLightView.alpha = currentAlpha
         yellowLightView.alpha = currentAlpha
         greenLightView.alpha = currentAlpha
+        redLightView.layer.cornerRadius = 65
+        yellowLightView.layer.cornerRadius = 65
+        greenLightView.layer.cornerRadius = 65
         
         scrollingButton.configuration = setUpButton(with: "Start")
         
     }
 
     
-    @IBAction func buttonTapped(by user: UIButton) {
+    @IBAction func buttonTapped() {
         
+        scrollingButton.setTitle("Next", for: .normal)
         
+        if greenLightView.alpha == yellowLightView.alpha {
+            greenLightView.alpha = CGFloat(MAXFLOAT)
+            redLightView.alpha = currentAlpha
+        }
+        else if greenLightView.alpha > currentAlpha && yellowLightView.alpha == redLightView.alpha {
+            yellowLightView.alpha = CGFloat(MAXFLOAT)
+            greenLightView.alpha = currentAlpha
+        }
+        else if yellowLightView.alpha > currentAlpha && redLightView.alpha == greenLightView.alpha {
+            redLightView.alpha = CGFloat(MAXFLOAT)
+            yellowLightView.alpha = currentAlpha
+        }
+         return
     }
     
     
     private func setUpButton(with title : String) -> UIButton.Configuration {
         var buttonConfiguration = UIButton.Configuration.filled()
         buttonConfiguration.title = title
-        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.910154283, green: 0.4194336236, blue: 0.6157357693, alpha: 1)
+        buttonConfiguration.baseBackgroundColor = UIColor.blue
         buttonConfiguration.buttonSize = .large
         buttonConfiguration.cornerStyle = .large
-        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 32, weight: .bold)
         
         return buttonConfiguration
     }
